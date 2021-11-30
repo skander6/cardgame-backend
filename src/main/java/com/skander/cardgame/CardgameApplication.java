048a53454899940632202067c47dd9b1d69fb4b3
skander6 <khdhiriskander@gmail.com>
2021-11-30 12:28:54 +0100

    Adding Project structure

---
+++ src/main/java/com/skander/cardgame/CardgameApplication.java
@@ -0,0 +1,13 @@
+package com.skander.cardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardgameApplication.class, args);
+	}
+
+}
